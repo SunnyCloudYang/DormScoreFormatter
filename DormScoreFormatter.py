12470dd4af88b0de59c15eb474579592843b62f1
Yunyang <sunnycloudyang@outlook.com>
2024-09-29 15:21:24 +0800

    feat: support export to pdf, add more options

--- DormScoreFormatter.py
+++ DormScoreFormatter.py
@@ -4,7 +4,7 @@ import sys
 import pandas as pd
 import argparse
 from openpyxl import Workbook
-from openpyxl.styles import Alignment, Font, Border, Side
+from openpyxl.styles import Alignment, Font, Border, Side, PatternFill
 import re
 
 def load_and_process_csv_files(folder_path):
@@ -17,12 +17,14 @@ def load_and_process_csv_files(folder_path):
     
     combined_df = pd.concat(all_data, ignore_index=True)
     processed_df = combined_df[['楼号', '周', '房间', '床位', '总分', '整改意见']].copy()
+    processed_df = processed_df.drop_duplicates(subset=['楼号', '房间', '床位'], keep='first')
     processed_df['整改意见'] = processed_df['整改意见'].apply(lambda x: re.sub(r'[^\w\s]', '', str(x)))
     processed_df = processed_df.sort_values(['房间', '床位'])
     
     return processed_df
 
-def create_excel_file(df, output_file):
+def create_excel_file(df, output_file, email_prefix, folder_path):
+    no_error = True
     wb = Workbook()
     ws = wb.active
 
@@ -42,7 +44,7 @@ def create_excel_file(df, output_file):
     ws['A2'].alignment = Alignment(horizontal='center', vertical='center')
 
     ws.merge_cells('A3:H3')
-    ws['A3'] = "如有疑问请联系学生楼长：xxx@mails.tsinghua.edu.cn 或登陆家园网查询具体成绩"
+    ws['A3'] = f'如有疑问请联系学生楼长：{email_prefix}@mails.tsinghua.edu.cn 或登陆家园网查询具体成绩'
     ws['A3'].alignment = Alignment(horizontal='center', vertical='center')
 
     # Add data
@@ -71,8 +73,9 @@ def create_excel_file(df, output_file):
 
         for i, header in enumerate(headers):
             if data_row[header] == '' or pd.isna(data_row[header]) or data_row[header] == 'nan':
-                ws.cell(row=row, column=col+i, value='')
+                ws.cell(row=row, column=col+i, value='').fill = PatternFill(start_color="FF0000", end_color="FF0000", fill_type = "solid")
                 logging.warning(f"Empty cell at row {row}, column {col+i}: {header}: {data_row[header]}")
+                no_error = False
             else:
                 ws.cell(row=row, column=col+i, value=data_row[header])
             ws.cell(row=row, column=col+i).alignment = Alignment(horizontal='center', vertical='center')
@@ -87,17 +90,89 @@ def create_excel_file(df, output_file):
         for cell in row:
             cell.border = thin_border
     
-    wb.save(output_file)
+    wb.save(os.path.join(folder_path, output_file))
+    
+    return no_error
+    
+
+def generatePDF(folder_path, excel_file):
+    print("Generating PDF file...")
+    import win32com.client
+
+    excel = win32com.client.Dispatch('Excel.Application')
+    excel.Visible = False
+    try:
+        wb = excel.Workbooks.Open(os.path.join(
+            os.path.abspath(folder_path), excel_file))
+        ws = wb.Worksheets[0]
+        ws.PageSetup.HeaderMargin = excel.CentimetersToPoints(0.8)
+        ws.PageSetup.FooterMargin = excel.CentimetersToPoints(0.8)
+        ws.PageSetup.TopMargin = excel.CentimetersToPoints(1.6)
+        ws.PageSetup.BottomMargin = excel.CentimetersToPoints(1.6)
+        ws.PageSetup.FitToPagesWide = 1
+        ws.PageSetup.CenterHorizontally = True
+        pdf_file = os.path.splitext(excel_file)[0] + '.pdf'
+        wb.ExportAsFixedFormat(0, os.path.join(os.path.abspath(folder_path), pdf_file))
+        wb.Close(SaveChanges=True)
+        print(f"PDF file '{pdf_file}' has been created successfully.")
+    except Exception as e:
+        logging.error('Error occurred while generating PDF file.', e)
+    finally:
+        excel.Quit()
 
 def main():
-    parser = argparse.ArgumentParser(description='Process WeekScoreManage CSV files and create Excel report.')
-    parser.add_argument('--folder', default='.', help='Path to the folder containing CSV files')
+    parser = argparse.ArgumentParser(description='Process WeekScoreManage CSV files and create Excel and PDF report.')
+    parser.add_argument('--folder', default='.', help='path to the folder containing CSV files')
+    parser.add_argument('--email', default='xxx', help='prefix of your THU email address, like \'yunyang-21\'')
+    parser.add_argument('--pdf', default='true', help='whether to generate PDF file', choices=['true', 'false'])
+    parser.add_argument('--overwrite', default='false', help='overwrite existing excel files', choices=['true', 'false'])
+    parser.add_argument('--pdfOnly', default='false', help='generate PDF file only', choices=['true', 'false'])
+    parser.add_argument('--clean', default='false', help='clean up the csv files after success', choices=['true', 'false'])
     args = parser.parse_args()
+    
+    no_error = True
+    
+    if args.pdfOnly.lower() == 'true':
+        for file in os.listdir(args.folder):
+            if file.endswith('.xlsx'):
+                generatePDF(args.folder, file)
+        return
 
     df = load_and_process_csv_files(args.folder)
+    
+    # Check if the output file already exists
     output_file = f"{df['楼号'].iloc[0]}{df['周'].iloc[0]}.xlsx"
-    create_excel_file(df, output_file)
+    if os.path.exists(os.path.join(args.folder, output_file)) and args.overwrite.lower() == 'false':
+        print(f"Excel file '{output_file}' already exists. Please set '--overwrite true' to overwrite it.")
+    else:
+        no_error = create_excel_file(df, output_file, args.email, args.folder)
     print(f"Excel file '{output_file}' has been created successfully.")
+    
+    if args.pdf.lower() == 'true' and no_error:
+        generatePDF(args.folder, output_file)
+    elif not no_error:
+        confirm = input("There are empty cells in the Excel file. Do you still want to generate PDF file? (Y/[N]) ")
+        if confirm.lower() == 'y':
+            generatePDF(args.folder, output_file)
+        else:
+            print("PDF file will not be generated.")
+            
+    if args.clean.lower() == 'true' and no_error:
+        clean_up(args.folder)
+    elif not no_error:
+        confirm = input("There are empty cells in the Excel file. Do you still want to clean up the CSV files? (Y/[N]) ")
+        if confirm.lower() == 'y':
+            clean_up(args.folder)
+            print("CSV files have been cleaned up.")
+        else:
+            print("CSV files will not be cleaned up.")
+            
+def clean_up(folder_path):
+    print("Cleaning up CSV files...")
+    for file in os.listdir(folder_path):
+        if file.startswith('WeekScoreManage_') and file.endswith('.csv'):
+            os.remove(os.path.join(folder_path, file))
+    print("CSV files have been cleaned up.")
 
 if __name__ == '__main__':
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
